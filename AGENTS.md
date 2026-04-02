4df61744bb9214400c957df9b22b935548aa66e3
Dev-LDRC <lucasrabelocruz@gmail.com>
2026-04-02 17:54:26 -0300

    initial commit

---
+++ AGENTS.md
@@ -0,0 +1,17 @@
+# Este projeto é parte de um teste de seleção de frontend onde nós vamos construir uma landing page para o filme da Barbie, como documentado no `./README.md`, e esse projeto tem regras e orientações de como seguir.
+
+## Paleta de cores:
+- #F2EAD3
+- #DFD7BF
+- #333333
+- #F2F2F2
+- #102C5733
+- #3F2305
+- #A4907C
+
+## Observações:
+> Na seção de "Elenco", você vai inserir todos os atores do elenco, sendo que será carousel tanto no descktop como tablet e mobile. Para habilitar a função carousel, utilizamos está lib: https://owlcarousel2.github.io/OwlCarousel2/demos/demos.html mas fique a vontade para utilizar a que se sentir mais confortável.
+
+> As imagens estão na pasta chamada "imgs" enumerados de 1 a 9.
+
+> No botão "Ver mais" não precisa fazer nenhuma listagem, apenas a sua customização como sinalizada no layout.
---
+++ scripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+
+  // Mobile menu
+  $('.menu-toggle').on('click', function() {
+    const $nav = $('.mobile-nav');
+    const isHidden = $nav.attr('hidden') !== undefined;
+    
+    if (isHidden) {
+      $nav.removeAttr('hidden');
+      $(this).attr('aria-expanded', 'true');
+    } else {
+      $nav.attr('hidden', '');
+      $(this).attr('aria-expanded', 'false');
+    }
+  });
+
+  // Fechar o menu quando estiver em Desktop
+  $(window).on('resize', function() {
+    if ($(window).width() > 1024) {
+      const $nav = $('.mobile-nav');
+      if ($nav.attr('hidden') === undefined) {
+        $nav.attr('hidden', '');
+        $('.menu-toggle').attr('aria-expanded', 'false');
+      }
+    }
+  });
+
+  // Elenco Carousel
+  $('.cast-list').owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 7000,
+    responsive: {
+      0: {
+        items: 2
+      },
+      600: {
+        items: 3
+      },
+      1000: {
+        items: 6
+      }
+    }
+  });
+
+});
